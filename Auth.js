1178dd7f92158e22c5693a9f5e384200df2501a2
LarissaScarlat <alarissascarlat@gmail.com>
2025-12-05 16:14:47 -0300

    Erro da linha 119

--- Auth.js
+++ Auth.js
@@ -116,7 +116,7 @@ app.get("/callback", async (req, res) => {
 
 app.use("/", RouterRelatorio);
 app.use("/notificacoes-shopee", RouterPush);
-app.use("/",RouterRelatorio)
+app.use("/",RouterRelatorio);
 
 
 
