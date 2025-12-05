37db8fd8114dca63187289f5365bc9200c4c65f2
LarissaScarlat <alarissascarlat@gmail.com>
2025-12-05 11:30:20 -0300

    Ajustando o codigo e erros

--- Supabase.js
+++ Supabase.js
@@ -1,7 +1,9 @@
 // supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
