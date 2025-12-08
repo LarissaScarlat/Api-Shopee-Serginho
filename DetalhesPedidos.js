a6d15288c1bb9808bcb1b0cebed9ed42b1b84af2
LarissaScarlat <alarissascarlat@gmail.com>
2025-12-08 18:50:02 -0300

    ajustando

--- DetalhesPedidos.js
+++ DetalhesPedidos.js
@@ -47,7 +47,7 @@ async function consultarPedidoShopee(order_sn, access_token, shop_id) {
       .digest("hex");
 
     const url =
-      `https://openplatform.shopee.com${path}?` +
+      `https://openplatform.shopee.com.br${path}?` +
       `partner_id=${partner_id}&timestamp=${timestamp}&sign=${sign}&` +
       `access_token=${access_token}&shop_id=${shop_id}`;
 
