d0c5ae16e99d4dc4fee77f522376fdab76b8693a
LarissaScarlat <alarissascarlat@gmail.com>
2025-12-08 11:07:02 -0300

    ajustando o codigo

--- RelatorioPedidos_Saida.js
+++ RelatorioPedidos_Saida.js
@@ -1,5 +1,7 @@
-import express from "express";
-import axios from "axios";
+import express from 'express';
+import fs from 'fs';
+import axios from 'axios';
+import 'dotenv/config';
 
 const router = express.Router();
 
